4a364925a33da91c2135afeeccc113dd7f025661
Juan Camilo Rada Mesa <jcamilo.radamesa@aurea.com>
2017-01-26 10:15:15 -0500

    fixed nunit path

--- build-tools/scripts/run_ut.bat
+++ build-tools/scripts/run_ut.bat
@@ -1,1 +1,1 @@
-call ".\dotNet\build-tools\nunit\nunit3-console-x86.exe" .\dotNet\MathApp\bin\Debug\MathApp.exe
+call ".\dotNet\build-tools\nunit\nunit-console\nunit3-console.exe" .\dotNet\MathApp\bin\Debug\MathApp.exe
