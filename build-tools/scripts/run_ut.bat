962edf28b45fd09bed9c3559e306557556cb7d5c
Juan Camilo Rada <jcamilo.radamesa@aurea.com>
2017-01-26 14:28:24 +0000

    added nunit console local version

---
+++ build-tools/scripts/run_ut.bat
@@ -0,0 +1,1 @@
+call "..\nUnit\nunit3-console-x86.exe" .\dotNet\MathApp\bin\Debug\MathApp.exe
--- scripts/run_ut.bat
+++
@@ -1,1 +0,0 @@
-call "C:\Program Files (x86)\SharpDevelop\4.4\bin\Tools\NUnit\nunit-console-x86.exe" .\dotNet\MathApp\bin\Debug\MathApp.exe
